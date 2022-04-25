4416fa45af866ef49cb848f965c34ea7fbab1708
colinc27 <30709049+colinc27@users.noreply.github.com>
2022-04-24 18:50:10 -0700

    Update port

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 41186;
 const app = express();
 const apiRoutes = require('./routes/apiroutes');
 const htmlRoutes = require('./routes/htmlRoutes');
