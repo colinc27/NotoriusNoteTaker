088495f4354cb456d802ee2da23452047ebca9ec
colinc27 <30709049+colinc27@users.noreply.github.com>
2022-04-24 19:41:51 -0700

    port

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 
-const port = process.env.port || 41186;
+const port = process.env.port || 5000;
 const app = express();
 const apiRoutes = require('./routes/apiroutes');
 const htmlRoutes = require('./routes/htmlRoutes');
