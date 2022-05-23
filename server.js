61a0c45329d778b586f972d91a6c4005d2ad8f22
colinc27 <colinchristoph27@gmail.com>
2022-05-23 14:45:10 -0700

    changed

--- server.js
+++ server.js
@@ -2,16 +2,16 @@ const express = require('express');
 
 const port = process.env.port || 5000;
 const app = express();
-const apiRoutes = require('./routes/ApiRoutes');
-const htmlRoutes = require('./routes/HtmlRoutes');
+const apiroutes = require('./routes/apiroutes');
+const htmlroutes = require('./routes/htmlroutes');
 
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
 
-app.use('/api', apiRoutes);
-app.use('/', htmlRoutes);
+app.use('/api', apiroutes);
+app.use('/', htmlroutes);
 
 app.listen(port, () => {
   console.log(`API server now on port ${port}!`);
