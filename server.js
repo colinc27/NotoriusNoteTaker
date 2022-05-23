2541b894c67616637dd465a8c1947c8e8ee6c1ce
colinc27 <colinchristoph27@gmail.com>
2022-05-23 15:42:14 -0700

    server

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 
-const port = process.env.port || 5000;
+//const port = process.env.port || 5000;
 const app = express();
 const apiroutes = require('./routes/apiroutes');
 const htmlRoutes = require('./routes/htmlRoutes');
@@ -13,6 +13,6 @@ app.use(express.static('public'));
 app.use('/api', apiroutes);
 app.use('/', htmlRoutes);
 
-app.listen(port, () => {
-  console.log(`API server now on port ${port}!`);
+app.listen(process.env.PORT || 5000, '0.0.0.0', () => {
+  console.log("Server is running.");
 });
